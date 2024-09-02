011c56d374df6cf49344beddc30c9cc33103e18b
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-09-02 18:39:01 -0300

    SpringDoc Api Documentation

--- src/main/java/com/example/service/search/config/SpringDoc/SpringDocConfig.java
+++ src/main/java/com/example/service/search/config/SpringDoc/SpringDocConfig.java
@@ -3,6 +3,7 @@ package com.example.service.search.config.SpringDoc;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,11 @@ public class SpringDocConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .info(
+                        new Info().title("Search service api")
+                                .description("THIS PART OF THE API IS THE SEARCH AND DETAILED FILTERING PART")
+                                .version("v0.0.1")
+                )
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
                                 new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
--- src/main/java/com/example/service/search/controller/SearchController.java
+++ src/main/java/com/example/service/search/controller/SearchController.java
@@ -4,6 +4,7 @@ import com.example.service.search.model.ContractType;
 import com.example.service.search.model.DataPropertieSearch;
 import com.example.service.search.model.PropertyType;
 import com.example.service.search.service.SearchService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -22,6 +23,7 @@ public class SearchController {
     private SearchService service;
 
     @GetMapping("{contract}")
+    @Operation(summary ="Make a filtered search", description = "A filtered search with the main parameter being the type of contract you want, followed by the non-mandatory parameters being the type you want and the value")
     public ResponseEntity<Page<DataPropertieSearch>> search(@PathVariable(value = "contract") ContractType type, @RequestParam(name = "type",required = false) List<PropertyType> propertyType, @RequestParam(name = "price", required = false)String price, @PageableDefault(sort = {"id"}) Pageable pageable){
 
         return ResponseEntity.ok(service.search(type,propertyType,price,pageable));
