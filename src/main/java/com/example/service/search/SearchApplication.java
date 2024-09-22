44f25976712d8006555fe4386c7c5f1d997f598e
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-09-22 15:37:23 -0300

    Better Gateway Filters and Security Filter to receive authentication and implemented Feign to request method to validate token

--- src/main/java/com/example/service/search/SearchApplication.java
+++ src/main/java/com/example/service/search/SearchApplication.java
@@ -3,9 +3,11 @@ package com.example.service.search;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class SearchApplication {
 
 	public static void main(String[] args) {
